10186e7dd36d747ae5ece4e7f6716c48ccc5e8fb
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-06-26 23:39:59 -0300

    :rocket: science! Adicionando o básico de conexão com o Mongo, e o diagrama de classes.

---
+++ src/main/java/Main.java
@@ -0,0 +1,66 @@
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws UnknownHostException {
+        /**** Connect to MongoDB ****/
+        // Rodando no Docker
+        MongoClient mongo = new MongoClient("172.17.0.1", 27017);
+
+        /**** Get database ****/
+        // if database doesn't exists, MongoDB will create it for you
+        DB db = mongo.getDB("saep");
+
+        /**** Get collection / table from 'testdb' ****/
+        // if collection doesn't exists, MongoDB will create it for you
+        DBCollection table = db.getCollection("user");
+
+        /**** Insert ****/
+        // create a document to store key and value
+        BasicDBObject document = new BasicDBObject();
+        document.put("name", "mkyong");
+        document.put("age", 30);
+        document.put("createdDate", new Date());
+        table.insert(document);
+
+        /**** Find and display ****/
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("name", "mkyong");
+
+        DBCursor cursor = table.find(searchQuery);
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
+
+        /**** Update ****/
+        // search document where name="mkyong" and update it with new values
+        BasicDBObject query = new BasicDBObject();
+        query.put("name", "mkyong");
+
+        BasicDBObject newDocument = new BasicDBObject();
+        newDocument.put("name", "mkyong-updated");
+
+        BasicDBObject updateObj = new BasicDBObject();
+        updateObj.put("$set", newDocument);
+
+        table.update(query, updateObj);
+
+        /**** Find and display ****/
+        BasicDBObject searchQuery2
+                = new BasicDBObject().append("name", "mkyong-updated");
+
+        DBCursor cursor2 = table.find(searchQuery2);
+
+        while (cursor2.hasNext()) {
+            System.out.println(cursor2.next());
+        }
+
+        /**** Done ****/
+        System.out.println("Done");
+    }
+
+}
